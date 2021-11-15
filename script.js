492cfa1452f1e45e6c1c4123cf350fe702010359
allexqw <alexhotyan@gmail.com>
2021-11-15 16:49:00 +0300

    feat: add phone validation

--- script.js
+++ script.js
@@ -10,6 +10,72 @@ const items = document.querySelectorAll(".item");
 const itemButton = document.querySelectorAll(".items");
 const select = document.querySelectorAll(".select");
 
+const formInputPopup = document.querySelector(".request__form-input-popup");
+const formInput = document.querySelector(".request__form-input");
+const formField = document.querySelector(".request__form-field");
+const formFieldPopup = document.querySelector(".request__form-field-popup");
+
+let phoneNumber = "+375";
+let phone = false;
+document.addEventListener("keypress", (e) => {
+  console.log(phoneNumber.length);
+  if (document.querySelector(".popup.open")) {
+    formInputPopup.readOnly = true;
+    if (phoneNumber.length < 13) {
+      getNumber(e.key, formInputPopup);
+    } else {
+      getNumber("");
+    }
+  } else {
+    formInput.readOnly = true;
+    if (phoneNumber.length < 13) {
+      getNumber(e.key, formInput);
+    } else {
+      getNumber("");
+    }
+  }
+});
+
+function getNumber(number, input) {
+  phoneNumber = phoneNumber + number;
+  let form = phoneNumber.split("");
+  let formVal = [];
+  console.log(formVal);
+  for (let i = 0; i < form.length; i++) {
+    if (i == 4) {
+      formVal.push(" (");
+    }
+
+    if (i == 6) {
+      formVal.push(") ");
+    }
+
+    if (i == 9) {
+      formVal.push(" ");
+    }
+    if (i == 11) {
+      formVal.push(" ");
+    }
+
+    formVal.push(form[i]);
+  }
+  console.log(formVal);
+  console.log(formVal.join(""));
+  input.value = formVal.join("");
+}
+
+function validationPhone(phoneNumber) {
+  const regex = /^\+375((17 |25|29|33|44))[0-9]{3}[0-9]{2}[0-9]{2}$/;
+  console.log(phoneNumber);
+  if (regex.test(phoneNumber)) {
+    console.log(true);
+    phone = true;
+  } else {
+    phone = false;
+    console.log(false);
+  }
+}
+
 if (itemButton.length > 0) {
   itemButton.forEach((elem) => {
     const elements = elem.querySelectorAll(".select");
@@ -45,6 +111,7 @@ if (popupCloseIcon.length > 0) {
     const elem = popupCloseIcon[index];
     elem.addEventListener("click", (e) => {
       popupClose(elem.closest(".popup"));
+      setTimeout(bodyUnlock, 300);
     });
   }
 }
@@ -58,7 +125,11 @@ function bodyLock() {
   lockPadding.style.paddingRight = lockPaddingValue;
   body.classList.add("lock");
 }
-
+const invalidPhone = document.querySelector(".request__form-invalid-text");
+const invalidPhonePopup = document.querySelector(
+  ".request__form-invalid-text-popup"
+);
+const requestButton = document.querySelector(".request__form-button-items");
 function bodyUnlock() {
   lockPadding.style.paddingRight = "0px";
   body.style.paddingRight = "0px";
@@ -67,19 +138,55 @@ function bodyUnlock() {
 
 function popupOpen(currentPopup) {
   if (currentPopup) {
+    validationPhone(phoneNumber);
     const popupActive = document.querySelector(".popup.open");
-    if (popupActive) {
+    if (popupActive && phone == true) {
       popupClose(popupActive);
     } else {
       bodyLock();
     }
-    currentPopup.classList.add("open");
+    console.log(currentPopup);
+    if (currentPopup.id == "popup") {
+      if (phone) {
+        currentPopup.classList.add("open");
+        phoneNumber = "+375";
+        invalidRemove(invalidPhone, formInput, formField);
+        invalidRemove(invalidPhonePopup, formInputPopup, formFieldPopup);
+        formInput.value = "";
+        invalidPhone.style.display = "none";
+      } else {
+        console.log(invalidPhone);
+        invalidAdd(invalidPhone, formInput, formField);
+        invalidAdd(invalidPhonePopup, formInputPopup, formFieldPopup);
+      }
+    } else {
+      currentPopup.classList.add("open");
+      phoneNumber = "+375";
+      if (currentPopup.id == "popup-call") {
+        formInputPopup.focus();
+        formInputPopup.value = phoneNumber;
+      }
+    }
   }
+}
+
+function invalidAdd(text, input, field) {
+  text.style.display = "block";
+  phoneNumber = "+375";
+  input.value = phoneNumber;
+  input.classList.add("invalid");
+  field.classList.add("invalid");
+}
+function invalidRemove(text, input, field) {
+  text.style.display = "none";
+  phoneNumber = "+375";
+  input.value = phoneNumber;
+  input.classList.remove("invalid");
+  field.classList.remove("invalid");
 }
 
 function popupClose(popupActive) {
   popupActive.classList.remove("open");
-  setTimeout(bodyUnlock, 300);
 }
 
 buttonContact.addEventListener("click", (e) => {
@@ -148,7 +255,11 @@ if (menuLinks.length > 0) {
         top: linkBlockValue,
         behavior: "smooth",
       });
-
+      console.log(menuLink.outerText);
+      if (menuLink.outerText == "Заказать звонок") {
+        formInput.focus();
+        formInput.value = phoneNumber;
+      }
       e.preventDefault();
     }
   }
